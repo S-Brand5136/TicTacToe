032807ba7b2754c7776274c509b7e25650372fc1
S-Brand5136 <Brandon.Shemilt@live.co.uk>
2021-03-06 22:03:16 -0700

    add files

---
+++ Tictactoe/MarkType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// The type of valye a cell in the game is currently at
+
+namespace Tictactoe
+{
+    public enum MarkType
+    {
+        Free,
+        Nought,
+        Cross
+    }
+}
